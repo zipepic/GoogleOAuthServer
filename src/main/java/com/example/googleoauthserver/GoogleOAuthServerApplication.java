915f8f21ce6dc54e58e6c2eb9e1214f4770e6be6
xzz1p <olezhka.kirilov.97@mail.ru>
2024-01-27 00:29:25 +0300

    Initial commit

---
+++ src/main/java/com/example/googleoauthserver/GoogleOAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.googleoauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleOAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoogleOAuthServerApplication.class, args);
+    }
+
+}

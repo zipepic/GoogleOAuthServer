f750858db50435832c27932f3c723b728f62d9b7
Konstantin <75937074+zipepic@users.noreply.github.com>
2024-01-30 20:24:53 +0300

    Добавил шаблон получения id канала (#4)
    * Добавил шаблон получения id канала
    
    * Добавил зависимости для работы с google API
    
    * Реализовал логику получения id ютуб канала вошедшего пользователя

--- src/main/java/com/example/googleoauthserver/controllers/OAuthController.java
+++ src/main/java/com/example/googleoauthserver/controllers/OAuthController.java
@@ -1,21 +1,39 @@
 package com.example.googleoauthserver.controllers;
 
+import com.example.googleoauthserver.service.YoutubeChannelDataService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 @RestController
 public class OAuthController {
+    private final YoutubeChannelDataService youtubeChannelDataService;
+    @Autowired
+    public OAuthController(YoutubeChannelDataService youtubeChannelDataService) {
+        this.youtubeChannelDataService = youtubeChannelDataService;
+    }
+
     @GetMapping("/")
     public String getUserInfo(@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient) {
         Map<String,String> map = new HashMap<>();
         if(authorizedClient.getAccessToken()!= null){
+            try {
+                String youtubeChannelId = youtubeChannelDataService
+                        .getYoutubeChannelId(authorizedClient.getAccessToken().getTokenValue());
+                map.put("youtubeChannelId", youtubeChannelId);
+            } catch (IOException ex){
+                map.put("youtubeChannelIdException", ex.getMessage());
+            }
+
             map.put("access_token", authorizedClient.getAccessToken().getTokenValue());
             map.put("expires_at", authorizedClient.getAccessToken().getExpiresAt().toString());
             map.put("principal_name", authorizedClient.getPrincipalName());
+
         }
         if(authorizedClient.getRefreshToken()!=null){
             map.put("refresh_token", authorizedClient.getRefreshToken().getTokenValue());
